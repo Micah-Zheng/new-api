8fcce277cada3901cd45f2e99dfd3bec4923c3b8
Micah-Zheng <102610064+Micah-Zheng@users.noreply.github.com>
2026-05-12 22:03:15 +0800

    custom: force iframe reload on theme change

--- web/default/src/routes/_authenticated/status-monitor.tsx
+++ web/default/src/routes/_authenticated/status-monitor.tsx
@@ -15,7 +15,9 @@ function StatusMonitor() {
   return (
     <Main className='p-4'>
       <div className='bg-background min-h-0 flex-1 overflow-hidden rounded-xl border'>
+        {/* key={src} 确保主题切换时 iframe 重新加载，使 check-cx 读取新的 ?theme 参数 */}
         <iframe
+          key={src}
           src={src}
           title='Status Monitor'
           className='h-full w-full border-0'
