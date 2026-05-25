2e284d1499936a3c9dfc5649292ba4dc17d69c09
jkjk02 <scongwen14@gmail.com>
2026-05-25 14:44:32 +0800

    feat: redirect docs link to GitHub Pages documentation site
    Change default docs_link from docs.newapi.pro to our own
    GitHub Pages hosted documentation at micah-zheng.github.io/new-api/

--- setting/operation_setting/general_setting.go
+++ setting/operation_setting/general_setting.go
@@ -24,7 +24,7 @@ type GeneralSetting struct {
 
 // 默认配置
 var generalSetting = GeneralSetting{
-	DocsLink:                   "https://docs.newapi.pro",
+	DocsLink:                   "https://micah-zheng.github.io/new-api/",
 	PingIntervalEnabled:        false,
 	PingIntervalSeconds:        60,
 	QuotaDisplayType:           QuotaDisplayTypeUSD,
